3cd63da11ddd09c28e549eee38875ca4223f99f2
Prakarsh Kaushik <63556277+Prakarsh-Kaushik@users.noreply.github.com>
2023-01-30 00:48:21 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reverse words in a given string/reverse-words-in-a-given-string.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to reverse words in a given string.
+    string reverseWords(string S) 
+    { 
+        // code here 
+        string word;
+        string res;
+        stringstream ss(S);
+        
+        while(getline(ss,word, '.')){
+            res = word + "." + res;
+        }
+        res.pop_back();
+        return res;
+    } 
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+    int t;
+    cin >> t;
+    while (t--) 
+    {
+        string s;
+        cin >> s;
+        Solution obj;
+        cout<<obj.reverseWords(s)<<endl;
+    }
+}
+// } Driver Code Ends
